1b5beed69ef8157d6c340b69a2f47b7cd9684070
manu <pmrityunjay8888@gmail.com>
2020-06-03 17:18:55 +0530

    added update model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+def upload_updated_image(instance, filename):
+    return f'updates/{instance.user}/{filename}'
+class Update(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to=upload_updated_image, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.content or ""
