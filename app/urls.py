98bc358d713b0d04f78bc24092337037c8beecd6
manu <pmrityunjay8888@gmail.com>
2020-06-04 21:39:33 +0530

    added custom mixin for sending json response

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('sample', update_model_detail_view.as_view())
+    path('sample', json_example_view.as_view()),
+    path('sample_mixin',Json_example_using_mixin.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic import View
 from .models import Update
-class update_model_detail_view(View):
+import json
+from .mixins import JsonResponseMixin
+class json_example_view(View):
     """Just a sample view for checking JSONresponse"""
     data={
         "count":1000,
         "content":"Some new content"
     }
+    """Instead of using Json response earlier we used to do it with help of json dumps"""
+    """json_data = json.dumps(data)
+       return Httpreponse(json_data,content_type='application/json') 
+    """
     def get(self,request):
         return JsonResponse(self.data)
+
+
+class Json_example_using_mixin(JsonResponseMixin, View):
+    def get(self,reuqest,*args,**kwargs):
+        data = {
+            "count":100,
+            "content": "Some new content"
+        }
+        return self.render_to_json_response(data)
