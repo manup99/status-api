6dcc6e34eb3d9e4b4d46845cb17f2ef937431782
manu <pmrityunjay8888@gmail.com>
2020-06-27 00:31:50 +0530

    just some fixes

--- testing/crud_test.py
+++ testing/crud_test.py
@@ -5,9 +5,10 @@ ENDPOINT='http://127.0.0.1:8000/restapi/single_crudl'
 def do(method='get',data={},id=15):
     headers={}
     headers['content-type']='application/json'
+    data=json.dumps(data)
     r=requests.request(method,ENDPOINT+'?id='+str(id),data=data,headers=headers)
     print(r.text)
     return r
 
 
-do(data={'id':12})
+do(method='post',data={'content':'My name is Lakhan','user':1})
